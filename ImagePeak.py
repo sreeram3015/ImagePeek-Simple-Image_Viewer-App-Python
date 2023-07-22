af67ad7754b7b0eb1d8769194f343b829dc05472
Sreeram P <psreeram3015@proton.me>
2023-07-22 22:41:12 +0530

    It's late now. Just created a window. Some trouble in setting up the icon. Will look into it tomorrow. I think I should create a new branch to fix the icon and continue working on the project on master branch.

--- ImagePeak.py
+++ ImagePeak.py
@@ -1,6 +1,14 @@
 from tkinter import *
 
+from PIL import Image, ImageTk
+
 root= Tk()
+root.title("ImagePeak")
+# Load the icon image
+icon_image = PhotoImage(file='icon.png')  # Replace 'icon.png' with your icon image file
+# Set the icon for the application
+root.iconphoto(True, icon_image)
+root.geometry('600x600')
 
 
 root.mainloop()
