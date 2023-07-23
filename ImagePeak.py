4980991dcc351e05f5b9d9fab87ad60b2bbfc14f
Sreeram P <psreeram3015@proton.me>
2023-07-23 10:44:21 +0530

    Succesfully added the menu button

--- ImagePeak.py
+++ ImagePeak.py
@@ -10,5 +10,16 @@ icon_image = PhotoImage(file='icon.png')  # Replace 'icon.png' with your icon im
 root.iconphoto(True, icon_image)
 root.geometry('600x600')
 
+def popup_menu(e):
+    menu_bar.tk_popup(x=e.x_root, y=e.y_root)
+
+button_menu= Button(root, text= "≡", bd= 0, font= ('Bold', 30))
+button_menu.pack(side=TOP, anchor=W, pady= 20, padx= 10)
+button_menu.bind('<Button-1>', popup_menu)
+
+menu_bar = Menu(root, tearoff= False)
+menu_bar.add_command(label = 'Open Folder')
+
+
 
 root.mainloop()
