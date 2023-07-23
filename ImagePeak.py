c291b87ea6fd88bcd9ea6890246ac277c75edaaf
Sreeram P <psreeram3015@proton.me>
2023-07-23 11:16:12 +0530

    Still not used to git. Not understanding some parts of the code. But it's going well

--- ImagePeak.py
+++ ImagePeak.py
@@ -1,3 +1,5 @@
+import os
+import tkinter.filedialog as tk_file
 import tkinter.ttk as ttk
 from tkinter import *
 
@@ -13,6 +15,22 @@ root.geometry('600x600')
 
 def popup_menu(e):
     menu_bar.tk_popup(x=e.x_root, y=e.y_root)
+
+img_list = []
+img_vars = []
+
+def load_images():
+    path = tk_file.askdirectory()
+
+    img_files = os.listdir(path)
+
+    for r in range(0, len(img_files)):
+        img_list.append([
+            ImageTk.PhotoImage(Image.open(path+'/' +img_files[r]).resize((30,30),Image.ATIALIAS)),
+
+            ImageTk.PhotoImage(Image.open(path+'/' +img_files[r]).resize(400,300))
+        ])
+        img_vars.append(f'img_{r}')
 
 button_menu= Button(root, text= "≡", bd= 0, font= ('Bold', 30))
 button_menu.pack(side=TOP, anchor=W, pady= 20, padx= 10)
@@ -35,5 +53,6 @@ scroll_bar.config(command = canvas.xview)
 canvas.configure(xscrollcommand=scroll_bar.set)
 canvas.bind('<Configure>', lambda e: canvas.bbox('all'))
 
-
-root.mainloop()
+slider = Frame(canvas)
+canvas.create_window((0,0), window= slider, anchor= NW)
+root.mainloop()root.mainloop()
