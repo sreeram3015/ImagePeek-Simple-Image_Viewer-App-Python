a93a9e4f2e48e6890339aba1bbf9ca74b94136cf
Sreeram P <psreeram3015@proton.me>
2023-07-23 11:36:26 +0530

    A basic image viewer is done. Doing my own customizations

--- ImagePeak.py
+++ ImagePeak.py
@@ -18,26 +18,34 @@ def popup_menu(e):
 
 img_list = []
 img_vars = []
+VALID_IMAGE_EXTENSIONS = ('.png', '.jpg', '.jpeg')
+
+def display_images(index):
+    image_dp_label.config(image = img_list[index][1])
 
 def load_images():
     path = tk_file.askdirectory()
 
-    img_files = os.listdir(path)
+    img_files = img_files = [f for f in os.listdir(path) if os.path.splitext(f)[1].lower() in VALID_IMAGE_EXTENSIONS]
 
     for r in range(0, len(img_files)):
         img_list.append([
-            ImageTk.PhotoImage(Image.open(path+'/' +img_files[r]).resize((30,30),Image.ATIALIAS)),
+            ImageTk.PhotoImage(Image.open(path+'/' +img_files[r]).resize((30,30), Image.BILINEAR)),
 
-            ImageTk.PhotoImage(Image.open(path+'/' +img_files[r]).resize(400,300))
+            ImageTk.PhotoImage(Image.open(path+'/' +img_files[r]).resize((400,300), Image.BILINEAR))
         ])
         img_vars.append(f'img_{r}')
+
+    for n in range(len(img_vars)):
+        globals()[img_vars[n]] = Button(slider, image= img_list[n][0], bd=0, command= lambda n=n: display_images(n))
+        globals()[img_vars[n]].pack(side = LEFT)
 
 button_menu= Button(root, text= "≡", bd= 0, font= ('Bold', 30))
 button_menu.pack(side=TOP, anchor=W, pady= 20, padx= 10)
 button_menu.bind('<Button-1>', popup_menu)
 
 menu_bar = Menu(root, tearoff= False)
-menu_bar.add_command(label = 'Open Folder')
+menu_bar.add_command(label = 'Open Folder', command = load_images)
 
 image_dp_label = Label(root)
 image_dp_label.pack(anchor= CENTER)
@@ -55,4 +63,4 @@ canvas.bind('<Configure>', lambda e: canvas.bbox('all'))
 
 slider = Frame(canvas)
 canvas.create_window((0,0), window= slider, anchor= NW)
-root.mainloop()root.mainloop()
+root.mainloop()
